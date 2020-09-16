0c0697b9d3b2923260dadf0c2fb362b6e9e1b55b
Yuriy <tsarenkoyuriidev@gmail.com>
2020-09-16 18:49:41 +0300

    DEVELOPMENT-3: performed receiving of user location, added current weather layout.

--- app/src/main/java/com/example/weather4you/api/WeatherApi.java
+++ app/src/main/java/com/example/weather4you/api/WeatherApi.java
@@ -1,6 +1,9 @@
 package com.example.weather4you.api;
 
+import android.location.Location;
+
 public class WeatherApi {
 
     private static final String API_KEY = "get your api key from https://openweathermap.org";
+    public static Location current_Location = null;
 }
